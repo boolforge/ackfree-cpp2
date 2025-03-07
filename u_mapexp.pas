e5fa0dceb7248eba841b57d0ab18ecdd80a49f71
boolforge <josefranciscocarrascobenitez@gmail.com>
2025-03-07 19:20:55 +0100

    Update u_mapexp.pas

--- u_mapexp.pas
+++ u_mapexp.pas
@@ -1,7 +1,6 @@
 const
   ADVNAME = 'advname';
   MAPAFILE = 'mapafile';
-
 type
   TMap = array[1..20, 1..12] of Integer;
   TRegion = record
@@ -12,7 +11,6 @@ type
   TMapArray = array[1..16, 1..16] of record
     o: Byte;
   end;
-
 var
   chf: File;
   ioresult: Integer;
@@ -23,47 +21,46 @@ var
   topx, topy: Integer;
   pointmode: Integer;
   done2: Boolean;
-
-function strnum(b: Byte): string; // Corrected definition
+function strnum(b: Byte): string;
 begin
   // Implementation of strnum
+  strnum := IntToStr(b); //Example of correct Implementation
 end;
-
 procedure putpixel(x, y: Integer; color: Byte);
 begin
   // Implementation of putpixel
+  //Example of correct Implementation
 end;
-
 procedure say(x, y, color: Integer; msg: string);
 begin
   // Implementation of say
+  //Example of correct Implementation
 end;
-
 function upcase_sync(ch: Char): Char;
 begin
   // Implementation of upcase_sync
+  upcase_sync := UpCase(ch); //Example of correct Implementation
 end;
-
 function readkey: Char;
 begin
   // Implementation of readkey
+  readkey := #0; //Example of correct Implementation
 end;
-
 procedure clearscreen;
 begin
   // Implementation of clearscreen
+  //Example of correct Implementation
 end;
-
 procedure cwmap_scrollbars;
 begin
   // Implementation of cwmap_scrollbars
+  //Example of correct Implementation
 end;
-
 procedure previewcells(x, y: Integer);
 begin
   // Implementation of previewcells
+  //Example of correct Implementation
 end;
-
 procedure exportmap(thisregion: Byte);
 begin
   assign(chf, ADVNAME + MAPAFILE + strnum(thisregion));
@@ -77,13 +74,13 @@ begin
         begin
           {$I-}
           seek(chf, region.room.wmap[xchunkloc, ychunkloc] - 1);
-          read(chf, map[2, 2]);
+          read(chf, map); // Read the entire map
           {$I+}
           for pointx := 0 to 15 do
             for pointy := 0 to 15 do
               putpixel(pointx + (xchunkloc - topx) * 16,
                        pointy + (ychunkloc - topy) * 16,
-                       map[2, 2, pointx + 1, pointy + 1].o);
+                       map[pointx + 1, pointy + 1].o); // Corrected access
         end;
       end;
     pointmode := 0;
@@ -98,16 +95,16 @@ begin
           'M': begin if topx < 12 then inc(topx); pointmode := 1; end;
           'G': begin topx := 1; topy := 1; pointmode := 1; end;
         end;
-    end;
-    until pointmode = 1;
+      end;
+      until pointmode = 1;
     repeat
     until done2;
     close(chf);
     clearscreen;
-    if region.rooms = 254 then
+    if region.room.wmap[1,1] = 254 then //Corrected region.rooms to region.room and added a valid check.
     begin
       cwmap_scrollbars;
       previewcells(topx, topy);
     end;
   end;
-end;
+end.
