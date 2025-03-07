4836be9288c3af1f8a8ed0ac47af346793d9ba16
boolforge <josefranciscocarrascobenitez@gmail.com>
2025-03-07 18:57:11 +0100

    Update u_mapexp.pas

--- u_mapexp.pas
+++ u_mapexp.pas
@@ -1,31 +1,87 @@
-procedure exportmap(thisregion:byte);
+const
+  ADVNAME = 'advname';
+  MAPAFILE = 'mapafile';
 
-begin {alt-V}
+type
+  TMap = array[1..20, 1..12] of Integer;
+  TRegion = record
+    room: record
+      wmap: TMap;
+    end;
+  end;
+  TMapArray = array[1..16, 1..16] of record
+    o: Byte;
+  end;
 
-  assign(chf,ADVNAME+MAPAFILE+strnum(thisregion));
+var
+  chf: File;
+  strnum: function(byte): string;
+  ioresult: Integer;
+  region: TRegion;
+  xchunkloc, ychunkloc: Integer;
+  map: TMapArray;
+  pointx, pointy: Integer;
+  topx, topy: Integer;
+  pointmode: Integer;
+  done2: Boolean;
+
+procedure putpixel(x, y: Integer; color: Byte);
+begin
+  // Implementation of putpixel
+end;
+
+procedure say(x, y, color: Integer; msg: string);
+begin
+  // Implementation of say
+end;
+
+function upcase_sync(ch: Char): Char;
+begin
+  // Implementation of upcase_sync
+end;
+
+function readkey: Char;
+begin
+  // Implementation of readkey
+end;
+
+procedure clearscreen;
+begin
+  // Implementation of clearscreen
+end;
+
+procedure cwmap_scrollbars;
+begin
+  // Implementation of cwmap_scrollbars
+end;
+
+procedure previewcells(x, y: Integer);
+begin
+  // Implementation of previewcells
+end;
+
+procedure exportmap(thisregion: Byte);
+begin
+  assign(chf, ADVNAME + MAPAFILE + strnum(thisregion));
   {$I-} reset(chf); {$I+}
-  if ioresult=0 then
+  if ioresult = 0 then
   begin
     for xchunkloc := 1 to 20 do
       for ychunkloc := 1 to 12 do
       begin
-
-        if region.room.wmap[xchunkloc,ychunkloc] <> 0 then
+        if region.room.wmap[xchunkloc, ychunkloc] <> 0 then
         begin
           {$I-}
-          seek(chf, region.room.wmap[xchunkloc,ychunkloc] - 1);
-          read(chf, map[2,2]);
+          seek(chf, region.room.wmap[xchunkloc, ychunkloc] - 1);
+          read(chf, map[2, 2]);
           {$I+}
           for pointx := 0 to 15 do
             for pointy := 0 to 15 do
               putpixel(pointx + (xchunkloc - topx) * 16,
                        pointy + (ychunkloc - topy) * 16,
-                       map[2,2,pointx + 1,pointy + 1].o);
+                       map[2, 2, pointx + 1, pointy + 1].o);
         end;
-
       end;
-
-    end;
     pointmode := 0;
     say(2, 191, 0, 'ESC:EXIT  ARROW KEYS:MOVE');
     repeat
@@ -38,10 +94,10 @@ begin {alt-V}
           'M': begin if topx < 12 then inc(topx); pointmode := 1; end;
           'G': begin topx := 1; topy := 1; pointmode := 1; end;
         end;
-      end;
-      until pointmode = 1;
+    end;
+    until pointmode = 1;
     repeat
-      until done2;
+    until done2;
     close(chf);
     clearscreen;
     if region.rooms = 254 then
@@ -49,5 +105,5 @@ begin {alt-V}
       cwmap_scrollbars;
       previewcells(topx, topy);
     end;
-
   end;
+end;
